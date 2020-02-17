394b61d5c0fdb6db8982bfc051d393306030b941
chomeko3o <kusyopi@gmail.com>
2020-02-17 21:17:43 +0900

    リストに追加する為のメソッド

--- src/components/todo.js
+++ src/components/todo.js
@@ -9,17 +9,27 @@ class Todo extends React.Component {
       name: ''
     };
   }
+  //追加メソッドを定義
+  onInput = (e) => {
+    //nameの値を更新する
+    this.setState({
+      //入力された値をe.target.valueで取り出せる
+      name: e.target.value
+    });
+  }
+
   render() {
     // todosに初期値の空の配列とnameの空を代入
     const { todos } = this.state;
     //変数todosを.mapメソッドでtodoの値とindex番号に分ける
     //それをli要素のkeyにindex番号を持たし、変数todoの値をliで表示する
     return (
+      //入力された値をonInputメソッドでonInputに代入
       <div>
-        <input type="text" />
+        <input type="text" onInput={this.onInput} />
         <button>登録</button>
         <ul>
-          {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+          {todos.map((todo, index) => <li key={index}>{todo}</li>)}
         </ul>
       </div>
     );
