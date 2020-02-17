6244c0722ab431a462bc8e2b334cffeac8b5f683
chomeko3o <kusyopi@gmail.com>
2020-02-17 21:38:58 +0900

    todoを登録するメソッド定義

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,16 +5,26 @@ class Todo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo: [],
+      todos: [],
       name: ''
     };
   }
-  //追加メソッドを定義
+  //todoを追加メソッドを定義
   onInput = (e) => {
     //nameの値を更新する
     this.setState({
       //入力された値をe.target.valueで取り出せる
       name: e.target.value
+    });
+  }
+  //todoを登録するメソッドを定義
+  addTodo = () => {
+    //初期値を代入
+    const { todos, name } = this.state;
+    //
+    this.setState({
+      //スプレッド演算子を使って、配列を複製し、入力された値をセット
+      todos: [...todos, name]
     });
   }
 
@@ -24,12 +34,15 @@ class Todo extends React.Component {
     //変数todosを.mapメソッドでtodoの値とindex番号に分ける
     //それをli要素のkeyにindex番号を持たし、変数todoの値をliで表示する
     return (
-      //入力された値をonInputメソッドでonInputに代入
+      //入力されたらonInputメソッド発動し
+      //ボタンをクリックしたらaddTodoメソッド発動で追加
       <div>
         <input type="text" onInput={this.onInput} />
-        <button>登録</button>
+        <button　onClick={this.addTodo}>登録</button>
         <ul>
-          {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+          {todos.map((todo, index) => <li key={index}>
+            {todo}
+          </li>)}
         </ul>
       </div>
     );
